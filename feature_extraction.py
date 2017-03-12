24353a8d0a5b055d97a4c671421104a1e0945e89
apalvai <anoopreddy06@gmail.com>
2017-03-12 13:58:03 -0700

    Implemented the model that transfers learning from already trained networks like VGG, Inception & ResNet.

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,12 @@
 import pickle
 import tensorflow as tf
+import numpy as np
 # TODO: import Keras layers you need here
+import keras as K
+from keras.models import Model
+from keras.layers import Input
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from sklearn.preprocessing import LabelBinarizer
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -40,14 +46,37 @@ def main(_):
 
     print(X_train.shape, y_train.shape)
     print(X_val.shape, y_val.shape)
+    
+    # preprocess data
+    y_train = y_train.reshape((-1, 1))
 
     # TODO: define your model and hyperparams here
     # make sure to adjust the number of classes based on
     # the dataset
     # 10 for cifar10
     # 43 for traffic
-
+    nb_examples = X_train.shape[0]
+    nb_classes = len(np.unique(y_train))
+    input_shape = X_train[0].shape
+    
+    # model
+    input = Input(input_shape)
+    x = Flatten()(input)
+    x = Dense(nb_classes, activation='softmax')(x)
+    model = Model(input, x)
+    model.compile('adam', 'sparse_categorical_crossentropy', ['accuracy'])
+    
     # TODO: train your model here
+    history = model.fit(X_train, y_train, nb_epoch=100, validation_split=0.33)
+    
+    # evaluate
+    y_val = y_val.reshape((-1, 1))
+
+    metrics = model.evaluate(X_val, y_val)
+    for metric_i in range(len(model.metrics_names)):
+        metric_name = model.metrics_names[metric_i]
+        metric_value = metrics[metric_i]
+        print('{}: {}'.format(metric_name, metric_value))
 
 
 # parses flags and calls the `main` function above
